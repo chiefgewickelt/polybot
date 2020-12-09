796b7637b63ed258dbe862ce69ff171f89242248
JuliusR <>
2020-12-09 11:52:03 +0100

    add application state; log APM

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,55 @@
+const ACTIONS = {
+    MOUSE_MOVE: 'MOUSE_MOVE',
+    CLICK: 'CLICK',
+};
 
+function createMouseMoveAction(e) {
+    return {
+        type: ACTIONS.MOUSE_MOVE,
+        mousePos: {
+            x: e.clientX,
+            y: e.clientY,
+        },
+    };
+}
 
+function createMouseClickAction(e) {
+    return {
+        type: ACTIONS.CLICK,
+    };
+}
 
+function stateReducer(state, action) {
+    switch (action.type) {
+        case ACTIONS.MOUSE_MOVE:
+            return { ...state, mousePos: action.mousePos };
+        case ACTIONS.CLICK:
+            const totalNumberOfClicks = state.totalNumberOfClicks + 1;
+            return { ...state, totalNumberOfClicks };
+        default:
+            throw new Error(`unknown action ${action.type}`);
+    }
+}
+
+function makeState(reducer, makeInitialState) {
+    if (!makeInitialState) {
+        makeInitialState = () => ({});
+    }
+    const state = { current: makeInitialState() };
+    const dispatch = action => {
+        state.current = reducer(state.current, action);
+    };
+    return [state, dispatch];
+}
+
+const [state, dispatch] = makeState(stateReducer, () => ({
+    name: 'Player',
+    totalNumberOfClicks: 0,
+    gameStartedAt: Date.now(),
+}));
 
 window.onload = () => {
+    console.log(state);
     const canvas = createCanvas();
     document.body.append(canvas);
     console.log('foo!');
@@ -14,22 +61,27 @@ function createCanvas() {
     canvas.id = 'root';
 
     canvas.addEventListener('click', e => {
-        console.log('clicked on canvas', e);
+        dispatch(createMouseClickAction(e));
+        console.log('clickCount', state.current.totalNumberOfClicks);
+        console.log('when?', state.current.gameStartedAt);
+        const elapsedMinutes = (Date.now() - state.current.gameStartedAt) / (1000 * 60);
+        const apm = state.current.totalNumberOfClicks / elapsedMinutes;
+        console.log('APM', apm);
     });
 
     canvas.addEventListener('mousemove', e => {
-        console.log('mouse moven on canvas', e);
+        dispatch(createMouseMoveAction(e));
     });
 
     return canvas;
 }
 
-function draw_line(x1,y1,x2,y2) {
+function draw_line(x1, y1, x2, y2) {
     const c = document.getElementById('root');
     const ctx = c.getContext('2d');
     ctx.beginPath();
-    ctx.moveTo(x1,y1)
-    ctx.lineTo(x2,y2);
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2);
     ctx.closePath();
     ctx.stroke();
 }
