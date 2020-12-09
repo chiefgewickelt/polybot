b60fc7aaa2712679bdd230c2f6d0dbf04ef0ddf4
JuliusR <>
2020-12-09 10:57:35 +0100

    add click and mousemove events

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
+
+
+
+
 window.onload = () => {
-    const canvas = document.createElement("canvas");
-    canvas.id = 'root';
+    const canvas = createCanvas();
     document.body.append(canvas);
     console.log('foo!');
 };
 
+function createCanvas() {
+    const canvas = document.createElement("canvas");
+    canvas.id = 'root';
+
+    canvas.addEventListener('click', e => {
+        console.log('clicked on canvas', e);
+    });
+
+    canvas.addEventListener('mousemove', e => {
+        console.log('mouse moven on canvas', e);
+    });
+
+    return canvas;
+}
